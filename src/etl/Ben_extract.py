de3c6a5d2de15f37b60b21ddbbad6e820a7bce5a
Ben <ben.marsh304@gmail.com>
2023-10-11 15:13:44 -0700

    Added ratings data from interactions csv.

--- src/etl/Ben_extract.py
+++ src/etl/Ben_extract.py
@@ -1,26 +1,28 @@
 import pandas as pd
+import numpy as np
 import kaggle
-from zipfile import ZipFile
 from ast import literal_eval
 
 # Download data
 print('Downloading data from Kaggle...')
 kaggle.api.authenticate()
-kaggle.api.dataset_download_file(dataset='shuyangli94/food-com-recipes-and-user-interactions/',
-                                 file_name='RAW_recipes.csv',
-                                 path='../../data/')
-
-# Unzip data file
-with ZipFile('../../data/RAW_recipes.csv.zip', 'r') as zip:
-    zip.extractall('../../data/')
+kaggle.api.dataset_download_files(dataset='shuyangli94/food-com-recipes-and-user-interactions/',
+                                  path='../../data/',
+                                  unzip=True)
 
 # Import data to pandas
 print('Processing data...')
-raw_df = pd.read_csv('../../data/RAW_recipes.csv',
-                     converters={'tags': literal_eval,
-                                 'nutrition': literal_eval,
-                                 'steps': literal_eval,
-                                 'ingredients': literal_eval})
+recipe_df = pd.read_csv('../../data/RAW_recipes.csv',
+                        converters={'tags': literal_eval,
+                                    'nutrition': literal_eval,
+                                    'steps': literal_eval,
+                                    'ingredients': literal_eval})
+
+interactions_df = pd.read_csv('../../data/RAW_interactions.csv')
+
+# Calculate average rating for each recipe. Add this to recipe_df
+avg_rating = interactions_df.groupby(by='recipe_id', as_index=False)['rating'].agg(np.mean)
+recipe_df = pd.merge(recipe_df, avg_rating, how='left', left_on='id', right_on="recipe_id")
 
 
 def nutrition_labels(nutrition):
@@ -46,7 +48,7 @@ def numbered_steps(steps):
         result += f'{i+1} {step}\n'
     return result
 
-#add col description to doc
+
 def create_doc(row):
     """
     Collapses each row into a single document for the recipe,
@@ -54,6 +56,7 @@ def create_doc(row):
     """
     # Extract and format relevant data from each field
     name = row['name']
+    rating = round(row['rating'], 1)
     minutes = row['minutes']
     tags = ', '.join(row['tags'])
     description = row['description']
@@ -61,16 +64,16 @@ def create_doc(row):
     ingredients = ', '.join(row['ingredients'])
     steps = numbered_steps(row['steps'])    
     nutrition_info = nutrition_labels(row['nutrition'])
-    
+
     # Combine fields into full recipe    
-    full_recipe = f"Name: {name}\n\nCook Time: {minutes} minutes\n\nTags: {tags}\n\nDescription: {description}\n\nNumber of ingredients: {n_ingredients}\n\nIngredients List: {ingredients}\n\nSteps:\n{steps}\n\nNutrition: {nutrition_info}"
+    full_recipe = f"Name: {name}\n\nRating: {rating}/5\n\nCook Time: {minutes} minutes\n\nTags: {tags}\n\nDescription: {description}\n\nNumber of ingredients: {n_ingredients}\n\nIngredients List: {ingredients}\n\nSteps:\n{steps}\n\nNutrition: {nutrition_info}"
     return full_recipe
 
 
 # Create single full_recipe doc for each row.
-raw_df['full_recipe'] = raw_df.apply(create_doc, axis=1)
+recipe_df['full_recipe'] = recipe_df.apply(create_doc, axis=1)
 
 # Create CSV of full recipe documents
 print('Saving prepared documents...')
-recipe_df = pd.DataFrame(raw_df['full_recipe'])
-recipe_df.to_csv('../../data/recipe_docs.csv', index=False)
+docs_df = pd.DataFrame(recipe_df[['name', 'full_recipe']])
+docs_df.to_csv('../../data/recipe_docs.csv', index=False)
