e9acd481f634e7f3f83fdd845c8e4e70d51d720c
Eva Draganova <45153242+evaplusai@users.noreply.github.com>
2023-10-29 12:47:20 -0400

    Squashed commit of the following:
    commit e50662cbdc77e966c7b4badcd013594935ca0cc3
    Merge: 7233bba 993b9ec
    Author: Eva Draganova <45153242+evaplusai@users.noreply.github.com>
    Date:   Mon Oct 23 15:22:06 2023 -0400
    
        Merge pull request #5 from btmarsh6/ETL
    
        baseline tests on local
    
    commit 993b9ec9bcafcc36d97d74bcc4ea88a8d29f5abf
    Author: Eva Draganova <45153242+evaplusai@users.noreply.github.com>
    Date:   Mon Oct 23 15:18:32 2023 -0400
    
        baseline tests on local
    
    commit 7233bba40b325acd1d3682733be8647678a843f9
    Merge: f46ad55 eefac0c
    Author: Laura Gutierrez Funderburk <lfunderburk@users.noreply.github.com>
    Date:   Tue Oct 17 15:31:55 2023 -0700
    
        Merge pull request #3 from btmarsh6/ETL
    
        Etl
    
    commit f46ad55eca8c1d7b09f5a737431390b310669176
    Merge: 8d1997f d80f6f1
    Author: Laura Gutierrez Funderburk <lfunderburk@users.noreply.github.com>
    Date:   Tue Oct 17 15:30:17 2023 -0700
    
        Merge pull request #2 from btmarsh6/dev-eva
    
        add extract from kaggle and save to duckDB
    
    commit eefac0c4c6df4ff0ff23b4f89ad394c7ff926e64
    Author: Ben <ben.marsh304@gmail.com>
    Date:   Mon Oct 16 12:08:56 2023 -0700
    
        removed row missing name from recipe_docs.csv
    
    commit 591e720724a96c7f3f4bbea64e3d239dbf47e9bd
    Author: Ben <ben.marsh304@gmail.com>
    Date:   Thu Oct 12 16:46:50 2023 -0700
    
        built faiss doc store file with functions to initiate documents, document store, retriever and pipeline. app.py file to implement chatbot.
    
    commit de3c6a5d2de15f37b60b21ddbbad6e820a7bce5a
    Author: Ben <ben.marsh304@gmail.com>
    Date:   Wed Oct 11 15:13:44 2023 -0700
    
        Added ratings data from interactions csv.
    
    commit 7c0c4c14c80e69c97cf9ba34ccdc6156996a0b7a
    Author: Ben <ben.marsh304@gmail.com>
    Date:   Mon Oct 9 10:10:24 2023 -0700
    
        Added description field into extraction process.
    
    commit 8d1997f2ce32fdd1da77800369f02781e1133e2e
    Merge: 0ddae1e ab846a8
    Author: Ben <126756817+btmarsh6@users.noreply.github.com>
    Date:   Sun Oct 8 16:15:51 2023 -0700
    
        Merge pull request #1 from btmarsh6/ETL
    
        Built data extraction pipeline. Downloads data and combines fields in…
    
    commit ab846a87f0b09106a05a5796a7cf65d02164dc11
    Author: Ben <ben.marsh304@gmail.com>
    Date:   Sun Oct 8 16:14:24 2023 -0700
    
        Built data extraction pipeline. Downloads data and combines fields into full recipe documents.

---
+++ src/etl/Ben_extract.py
@@ -0,0 +1,82 @@
+import pandas as pd
+import numpy as np
+import kaggle
+from ast import literal_eval
+
+# Download data
+print('Downloading data from Kaggle...')
+kaggle.api.authenticate()
+kaggle.api.dataset_download_files(dataset='shuyangli94/food-com-recipes-and-user-interactions/',
+                                  path='../../data/',
+                                  unzip=True)
+
+# Import data to pandas
+print('Processing data...')
+recipe_df = pd.read_csv('../../data/RAW_recipes.csv',
+                        converters={'tags': literal_eval,
+                                    'nutrition': literal_eval,
+                                    'steps': literal_eval,
+                                    'ingredients': literal_eval})
+
+interactions_df = pd.read_csv('../../data/RAW_interactions.csv')
+
+# Calculate average rating for each recipe. Add this to recipe_df
+avg_rating = interactions_df.groupby(by='recipe_id', as_index=False)['rating'].agg(np.mean)
+recipe_df = pd.merge(recipe_df, avg_rating, how='left', left_on='id', right_on="recipe_id")
+
+
+def nutrition_labels(nutrition):
+    """ (list) -> str
+    Takes list of nutrition information from dataframe and adds labels.
+    """
+    nutrition_labels = [' calories', '% total fat', '% sugar',
+                        '% sodium', '% protein', '% saturated fat',
+                        '% total carbohydrates']
+    result = ''
+    for i in range(6):
+        labelled = f'{nutrition[i]}{nutrition_labels[i]}, '
+        result += labelled
+    return result
+
+
+def numbered_steps(steps):
+    """ (list) -> str
+    Takes list of steps and converts to a string of numbered steps. 
+    """
+    result = ''
+    for i, step in enumerate(steps):
+        result += f'{i+1} {step}\n'
+    return result
+
+
+def create_doc(row):
+    """
+    Collapses each row into a single document for the recipe,
+    adding labels to column values.
+    """
+    # Extract and format relevant data from each field
+    name = row['name']
+    rating = round(row['rating'], 1)
+    minutes = row['minutes']
+    tags = ', '.join(row['tags'])
+    description = row['description']
+    n_ingredients = row['n_ingredients']
+    ingredients = ', '.join(row['ingredients'])
+    steps = numbered_steps(row['steps'])    
+    nutrition_info = nutrition_labels(row['nutrition'])
+
+    # Combine fields into full recipe    
+    full_recipe = f"Name: {name}\n\nRating: {rating}/5\n\nCook Time: {minutes} minutes\n\nTags: {tags}\n\nDescription: {description}\n\nNumber of ingredients: {n_ingredients}\n\nIngredients List: {ingredients}\n\nSteps:\n{steps}\n\nNutrition: {nutrition_info}"
+    return full_recipe
+
+
+# Create single full_recipe doc for each row.
+recipe_df['full_recipe'] = recipe_df.apply(create_doc, axis=1)
+docs_df = pd.DataFrame(recipe_df[['name', 'full_recipe']])
+
+# Drop row with missing name
+docs_df.dropna(inplace=True)
+
+# Create CSV of full recipe documents
+print('Saving prepared documents...')
+docs_df.to_csv('../../data/recipe_docs.csv', index=False)
